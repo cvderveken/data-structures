6155a10bb244e221fea88a0f6278b9fd42f92a2f
Cristophe Van der Veken <daefetd@gmail.com>
2024-08-13 19:55:42 +0200

    Create dstruct.cpp

---
+++ dstruct.cpp
@@ -0,0 +1,23 @@
+#include <list>
+#include <queue>
+
+// Linked List Implementation
+class LinkedList {
+public:
+    void insert(int value) { /* Implementation */ }
+    int deleteNode() { /* Implementation */ }
+};
+
+// Stack Implementation using Vector
+class Stack {
+public:
+    void push(int value) { /* Implementation */ }
+    int pop() { /* Implementation */ }
+};
+
+// Queue Implementation using Deque
+class Queue {
+public:
+    void enqueue(int value) { /* Implementation */ }
+    int dequeue() { /* Implementation */ }
+};
